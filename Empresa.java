06f0ed383e4f97d1a57eb610ce840fc219dca869
Eduardo Bardales <ebardalesdj@mpfn.gob.pe>
2023-03-22 14:14:55 -0500

    mensaje-descriptivo 2

--- Empresa.java
+++ Empresa.java
@@ -1,4 +1,22 @@
 public class Empresa {
 
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        // TODO Auto-generated method stub
+        super.finalize();
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
 
 }
